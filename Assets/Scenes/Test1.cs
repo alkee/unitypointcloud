4f620aa4844a92d6e7d3b194c914a8092dea57f6
alkee <alkee.na@gmail.com>
2021-06-07 20:53:16 +0900

    normal change rate 적용 resolve #7

--- Assets/Scenes/Test1.cs
+++ Assets/Scenes/Test1.cs
@@ -55,16 +55,15 @@ public class Test1
                 if (neighborIndices.Count < 3) { values[i] = float.NaN; return; }
 
                 var neighbors = pc.GetPoints(neighborIndices);
-                var plane = AnalysisTools.GetBestFittingPlane(neighbors);
+                var roughness = AnalysisTools.Roughness(p, neighbors);
 
-                var distance = Mathf.Abs(plane.GetDistanceToPoint(p));
-                if (min > distance) min = distance;
-                else if (max < distance) max = distance;
-                values[i] = distance;
+                if (min > roughness) min = roughness;
+                else if (max < roughness) max = roughness;
+                values[i] = roughness;
             }
             );
         }
-        Debug.Log($"roughness : min={min}, max={max}");
+        Debug.Log($"roughness : min ={min}, max={max}");
 
         // visualize
         var gradient = new Gradient();
@@ -85,6 +84,7 @@ public class Test1
         gradient.SetKeys(colorKeys, alphaKeys);
 
         var t = max - min;
+        if (t == 0) { Debug.LogError($"same values of roughness{min}"); return; }
         Parallel.For(0, count, (i) =>
         {
             var v = values[i];
@@ -96,23 +96,57 @@ public class Test1
 
     public void Test3()
     {
-        var samplIndex = 20000;
-        var p = pc.Points[samplIndex];
-        var pis = pc.GetPointIndices(p, 0.02f);
-        var points = pc.GetPoints(pis);
-        Debug.Log($"points found = {pis.Count}");
-        // coloring
-        foreach (var pi in pis) pcr.Colors[pi] = Color.green;
+        var min = float.MaxValue;
+        var max = float.MinValue;
+        var radius = 0.03f;
+
+        var count = pc.Points.Length;
+        var values = new float[count];
+        using (new ElapsedTimeLogger("calculating normal change rate"))
+        {
+            Parallel.For(0, count, (i) =>
+            {
+                var p = pc.Points[i];
+                var neighborIndices = pc.GetPointIndices(p, radius);
+                if (neighborIndices.Count < 4) { values[i] = float.NaN; return; }
+
+                var neighbors = pc.GetPoints(neighborIndices);
+                var normalChangeRate = AnalysisTools.NormalChangeRate(p, neighbors);
+
+                if (min > normalChangeRate) min = normalChangeRate;
+                else if (max < normalChangeRate) max = normalChangeRate;
+                values[i] = normalChangeRate;
+            }
+            );
+        }
+        Debug.Log($"normalChangeRate : min={min}, max={max}");
+
+        // visualize
+        var gradient = new Gradient();
+
+        // Populate the color keys at the relative time 0 and 1 (0 and 100%)
+        var colorKeys = new GradientColorKey[] {
+            new GradientColorKey { color = Color.blue, time = 0.0f },
+            new GradientColorKey { color = Color.green, time = 0.33f },
+            new GradientColorKey { color = Color.yellow, time = 0.66f },
+            new GradientColorKey { color = Color.red, time = 1.0f },
+        };
+        var alphaKeys = new GradientAlphaKey[]
+        {
+            new GradientAlphaKey { alpha = 1.0f, time = 0.0f },
+            new GradientAlphaKey { alpha = 1.0f, time = 1.0f },
+        };
+
+        gradient.SetKeys(colorKeys, alphaKeys);
+
+        var t = max - min;
+        if (t == 0) { Debug.LogError($"same values of normal change rate{min}"); return; }
+        Parallel.For(0, count, (i) =>
+        {
+            var v = values[i];
+            if (float.IsNaN(v)) { pcr.Colors[i] = Color.black; return; }
+            pcr.Colors[i] = gradient.Evaluate((v - min) / t);
+        });
         pcr.ApplyColors();
-
-        // find bestfitting plane
-        var plane = AnalysisTools.GetBestFittingPlane(points);
-        var pos = points.GetMeanVector();
-
-        // visualize the plane
-        var go = GameObject.CreatePrimitive(PrimitiveType.Plane);
-        go.GetComponent<MeshRenderer>().material.doubleSidedGI = true;
-        go.transform.up = plane.normal;
-        go.transform.position = pos;
     }
 }
--- Assets/Script/AnalysisTools.cs
+++ Assets/Script/AnalysisTools.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using Emgu.CV;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
@@ -6,8 +7,10 @@ namespace upc
 {
     public class AnalysisTools
     {
-        public static Plane GetBestFittingPlane(IEnumerable<Vector3> points)
+        public static Plane? GetBestFittingPlane(IEnumerable<Vector3> points)
         {
+            var count = points.Count();
+            if (count < 3) return null;
             var center = points.GetMeanVector();
             var mat = new Emgu.CV.Mat(3, points.Count(), Emgu.CV.CvEnum.DepthType.Cv32F, 1);
             var centered = points.Select(x => x - center);
@@ -15,6 +18,43 @@ namespace upc
             var svdU = mat.ComputeSvdU();
             var normal = svdU.GetVector3(2);
             return new Plane(normal, center);
+        }
+
+        public static float Roughness(Vector3 point, IEnumerable<Vector3> group)
+        {
+            var count = group.Count();
+            if (count == 3) return 0;
+            var plane = GetBestFittingPlane(group);
+            if (plane.HasValue == false) return float.NaN;
+            return Mathf.Abs(plane.Value.GetDistanceToPoint(point));
+        }
+
+        public static float NormalChangeRate(Vector3 point, IEnumerable<Vector3> group)
+        { // ref : https://github.com/CloudCompare/CCCoreLib/blob/be52fc2f9981a80cd457cd914f44f17f6ebf04f1/src/Neighbourhood.cpp#L980-L1018
+            var count = group.Count();
+            if (count == 3) return 0;
+            if (count < 3) return float.NaN;
+
+            var mat = new Mat(3, count, Emgu.CV.CvEnum.DepthType.Cv32F, 1);
+            var center = group.GetMeanVector();
+            var centered = group.Select(x => x - center);
+            mat.SetVector3Values(centered);
+
+            var svdW = new Mat();
+            var svdU = new Mat();
+            var svdV = new Mat();
+            CvInvoke.SVDecomp(mat, svdW, svdU, svdV, Emgu.CV.CvEnum.SvdFlag.Default);
+
+            var e1 = svdW.GetFloatValue(0, 0);
+            var eigenValue1 = e1 * e1;
+            var e2 = svdW.GetFloatValue(0, 1);
+            var eigenValue2 = e2 * e2;
+            var e3 = svdW.GetFloatValue(0, 2);
+            var eigenValue3 = e3 * e3;
+
+            var sum = eigenValue1 + eigenValue2 + eigenValue3;
+            var min = Mathf.Min(eigenValue1, eigenValue2, eigenValue3);
+            return min / sum;
         }
     }
 }
